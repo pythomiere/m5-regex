4d04859db63d38e62a89ef14bd9703861a8e9b6a
Pythomiere <Pythomiere@github.com>
2025-11-26 17:01:27 -0500

    Implemented all three methods

--- src/main/java/regex/Main.java
+++ src/main/java/regex/Main.java
@@ -38,9 +38,18 @@ public class Main {
      * @return whether the string satisfies the password requirements
      */
     public static boolean checkForPassword(String str, int minLength) {
-        final boolean propertyOne = Pattern.matches("REPLACE WITH CORRECT REGEX", str);
-        // as needed, modify this code.
-        return propertyOne;
+        if (str == null){
+            return false;
+        }
+        int strLength = str.length();
+        if (strLength < minLength){
+            return false;
+        }
+        final boolean uppercase = Pattern.matches(".*[A-Z].*", str);
+        final boolean lowercase = Pattern.matches(".*[a-z].*", str);
+        final boolean number = Pattern.matches(".*[0-9].*", str);
+        final boolean result = uppercase && lowercase && number;
+        return result;
     }
 
     // Method 2 for checking if a string conforms to a regex: using Matcher.find
@@ -55,7 +64,10 @@ public class Main {
      * @return a list containing the email addresses in the string.
      */
     public static List<String> extractEmails(String str) {
-        final Pattern pattern = Pattern.compile("REPLACE WITH CORRECT REGEX");
+        if (str == null){
+            return new ArrayList<>();
+        }
+        final Pattern pattern = Pattern.compile("[^@\\s]+@(mail\\.utoronto\\.ca|utoronto\\.ca)");
         final Matcher matcher = pattern.matcher(str);
         final List<String> result = new ArrayList<>();
         while (matcher.find()) {
@@ -76,6 +88,9 @@ public class Main {
      * @return whether str contains the same capital letter twice.
      */
     public static boolean checkForDoubles(String str) {
-        return str.matches("replace with correct regex");
+        if (str == null){
+            return false;
+        }
+        return str.matches(".*([A-Z]).*\\1.*");
     }
 }
